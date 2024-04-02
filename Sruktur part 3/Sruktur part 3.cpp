3efc417d20dddc091c61bd624ce38ab12a195c45
Kuroyukimaru <oranggabut712@gmail.com>
2024-04-02 11:04:46 +0700

    membuat getline dan ignore

---
+++ Sruktur part 3/Sruktur part 3.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat {
+	string desa;
+	string kota;
+};
+
+struct Mahasiswa {
+	string nim;
+	char nama;
+	DetailAlamat alamat;
+	int umur;
+};
+
+int main() {
+	Mahasiswa mhs;
+	cout << "Nomor Mahasiswa :";
+	cin >> mhs.nim;
+	cout << "Nama Mahasiswa :";
+	cin.getline(mhs.nama,25);
+	cin.ignore(1, '\n');
+
+	cout << "Alamat Mahasiswa :";
+	cout << "\t Nama Desa :";
+	cin >> mhs.alamat.desa;
+	cin.ignore(1, '\n');
+	cout << "\t Kota Mahasiswa :";
+	cin >> mhs.alamat.kota;
+	cin.ignore(1, '\n');
+
+	cout << "Umur Mahasiswa :";
+	cin >> mhs.umur;
+
+	cout << endl;
+	cout << "\n NIM :" << mhs.nim;
+	cout << "\n Nama :" << mhs.nama;
+	cout << "\n Alamat :" << endl;
+	cout << "\n \t Desa :" << mhs.alamat.desa;
+	cout << "\n \t Kota :" << mhs.alamat.kota;
+	cout << "\n Umur :" << mhs.umur;
+}
