486f6a0be7f17771db70977649ac4c8c598a9586
Kuroyukimaru <oranggabut712@gmail.com>
2024-04-02 10:45:20 +0700

    mengganti \t menjadi \n \t Desa

--- struktur alamat/struktur alamat.cpp
+++ struktur alamat/struktur alamat.cpp
@@ -33,7 +33,7 @@ int main() {
 	cout << "\n NIM :" << mhs.nim;
 	cout << "\n Nama :" << mhs.nama;
 	cout << "\n Alamat :" << endl;
-	cout << "\t Desa :" << mhs.alamat.desa;
+	cout << "\n \t Desa :" << mhs.alamat.desa;
 	cout << "\t Kota :" << mhs.alamat.kota;
 	cout << "\n Umur :" << mhs.umur;
 }
