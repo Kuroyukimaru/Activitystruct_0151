b1b1bc4abf8b3aaf29f6fd947f8de63af9ef5a4b
Kuroyukimaru <oranggabut712@gmail.com>
2024-04-02 11:19:17 +0700

    menambahkan for dan mhs[i]

---
+++ struktur part 4/struktur part 4.cpp
@@ -0,0 +1,47 @@
+#include <iostream>
+using namespace std;
+
+struct DetailAlamat {
+	string desa;
+	string kota;
+};
+
+struct Mahasiswa {
+	string nim;
+	string nama;
+	DetailAlamat alamat;
+	int umur;
+};
+
+int main() {
+	Mahasiswa mhs[3];
+	for (int i = 0, i < 3, i++) {
+		cout << "Nomor Mahasiswa :";
+		getline(cin, mhs[i].nim);
+		cout << "Nama Mahasiswa :";
+		getline(cin, mhs[i].nama);
+
+
+		cout << "Alamat Mahasiswa :";
+		cout << "\t Nama Desa :";
+		cin >> mhs[i].alamat.desa;
+
+		cout << "\t Kota Mahasiswa :";
+		cin >> mhs[i].alamat.kota;
+
+
+		cout << "Umur Mahasiswa :";
+		cin >> mhs[i].umur;
+
+	}
+	for (int i = 0, i < 3, i++) {
+		cout << endl;
+		cout << "\n NIM :" << mhs[i].nim;
+		cout << "\n Nama :" << mhs[i].nama;
+		cout << "\n Alamat :" << endl;
+		cout << "\n \t Desa :" << mhs[i].alamat.desa;
+		cout << "\n \t Kota :" << mhs[i].alamat.kota;
+		cout << "\n Umur :" << mhs[i].umur;
+
+	}
+}
