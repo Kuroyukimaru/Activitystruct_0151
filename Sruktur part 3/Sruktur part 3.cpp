835abf854ee50e269008f88cb05996d21da3d4bf
Kuroyukimaru <oranggabut712@gmail.com>
2024-04-02 11:12:03 +0700

    menambahkan getline

--- Sruktur part 3/Sruktur part 3.cpp
+++ Sruktur part 3/Sruktur part 3.cpp
@@ -16,7 +16,7 @@ struct Mahasiswa {
 int main() {
 	Mahasiswa mhs;
 	cout << "Nomor Mahasiswa :";
-	cin >> mhs.nim;
+	getline(cin, mhs.nim);
 	cout << "Nama Mahasiswa :";
 	getline(cin,mhs.nama);
 	
